2e0e42c88c2e4253b4add3de0d700145391b3fc6
taboo-hacker <leo43991314520@outlook.com>
2025-10-21 11:21:12 +0800

    更新GPLv3许可证

--- __init__.py
+++ __init__.py
@@ -1,4 +1,19 @@
-﻿from html import generate_html
+﻿# # -*- coding: utf-8 -*-
+# Copyright (C) 2025 taboo-hacker
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from html import generate_html
 from novel import novel_chapterizer
 from zip import extract_txt_from_zip
 __all__ = ["generate_html", "novel_chapterizer", "extract_txt_from_zip"]
